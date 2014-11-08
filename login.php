2e202202c7519ca02aae62b7dc1ca7b46502ddc7
unknown <Satyanarayana.gandham@telehealthindia.com>
2014-11-08 15:57:50 +0530

    added css & js

---
+++ login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Telerad Login</title>
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+<script href="bootstrap/cjs/bootstrap.min.js"/>
+<script href="jquery-1.11.1.min.js"/>
+</head>
+<body>
+<form id="loginForm" action="client.php" method="POST">
+<>
+</form>
+<?php
+
+?>
+
+</body>
+</html>
