87925f0c1ba96211d1e733a353c0db27a296cee8
unknown <Satyanarayana.gandham@telehealthindia.com>
2014-11-08 16:15:14 +0530

    format completed

--- login.php
+++ login.php
@@ -2,13 +2,14 @@
 <html>
 <head>
 <title>Telerad Login</title>
-<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"/>
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
 <script href="bootstrap/cjs/bootstrap.min.js"/>
 <script href="jquery-1.11.1.min.js"/>
 </head>
 <body>
 <form id="loginForm" action="client.php" method="POST">
-<>
+<input id="username" type="text" name="username" required>
+<input id="">
 </form>
 <?php
 
